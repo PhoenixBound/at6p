2b2e79d6039417568762a8b9981d61b84aa070b8
PhoenixBound <phoenixbound.github@gmail.com>
2025-02-10 16:17:02 -0600

    Add AT5P and earlier + Windows Unicode path support

--- 999decompress.c
+++ 999decompress.c
@@ -3,6 +3,30 @@
 // It has no bounds checking! I should not have written it in C
 // Please make something better than this
 
+// Naturally, almost all of the files in 999 have Japanese file names... so they're impossible to open
+// unless you handle Unicode paths.
+#ifdef _WIN32
+#define _UNICODE
+#include <fcntl.h>
+#include <io.h>
+#include <tchar.h>
+#define CHAR _TCHAR
+#define FOPEN _tfopen
+#define FPRINTF _ftprintf
+#define MAIN _tmain
+#define OUTPUT_UNICODE_ON_WINDOWS() do {   \
+    _setmode(_fileno(stderr), _O_U16TEXT); \
+} while (0)
+#define TEXT(...) _TEXT(__VA_ARGS__)
+#else
+#define CHAR char
+#define FOPEN fopen
+#define FPRINTF fprintf
+#define MAIN main
+#define OUTPUT_UNICODE_ON_WINDOWS() do {} while (0)
+#define TEXT(...) __VA_ARGS__
+#endif
+
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -126,50 +150,175 @@ bool decompress_at6p_data(uint8_t const *src /* r9 */, uint8_t *dest /* r10 */, 
 	return true;
 }
 
-void *decode_at_p_data(uint8_t const *in, uint32_t *out_size) {
-	uint32_t in_size = in[5] | ((unsigned)in[6] << 8);
-	uint32_t data_offset;
-	if (in[0] == 'A' && in[1] == 'T' && in[2] == '4' && in[3] == 'P') {
-		*out_size = in[0x10] | ((unsigned)in[0x11] << 8);
-		data_offset = 0x12;
-	} else if (in[0] == 'A' && in[1] == 'T' && in[2] == '5' && in[3] == 'P') {
-		*out_size = in[0x10] | ((unsigned)in[0x11] << 8) | ((uint32_t)in[0x12] << 16);
-		in_size |= in[0x13] << 16;
-		data_offset = 0x14;
-	} else if (in[0] == 'A' && in[1] == 'T' && in[2] == '3' && in[3] == 'P') {
-		data_offset = 0x10;
-		if (in[4] != 'N') {
+void *decode_at_p_data(uint8_t const *src, uint32_t *out_size) {
+	uint32_t src_size = src[5] | ((unsigned)src[6] << 8);
+	uint32_t src_index;
+    uint8_t *dest;
+    uint32_t dest_index = 0;
+    unsigned flag_byte = 0;
+    int bit_index = 8;
+    
+	if (src[0] == 'A' && src[1] == 'T' && src[2] == '4' && src[3] == 'P') {
+		*out_size = src[0x10] | ((unsigned)src[0x11] << 8);
+		src_index = 0x12;
+	} else if (src[0] == 'A' && src[1] == 'T' && src[2] == '5' && src[3] == 'P') {
+		*out_size = src[0x10] | ((unsigned)src[0x11] << 8) | ((uint32_t)src[0x12] << 16);
+		src_size |= src[0x13] << 16;
+		src_index = 0x14;
+	} else if (src[0] == 'A' && src[1] == 'T' && src[2] == '3' && src[3] == 'P') {
+		src_index = 0x10;
+		if (src[4] != 'N') {
 			fprintf(stderr, "%s: Formats that don't include the size like AT3P are not supported\n", __func__);
 			return NULL;
 		}
-	} else if (in[0] == 'A' && in[1] == 'T' && in[2] == '6' && in[3] == 'P') {
-		*out_size = in[0x10] | ((unsigned)in[0x11] << 8) | ((uint32_t)in[0x12] << 16);
-		uint8_t *data = malloc(*out_size);
-		if (!data) {
+	} else if (src[0] == 'A' && src[1] == 'T' && src[2] == '6' && src[3] == 'P') {
+		*out_size = src[0x10] | ((unsigned)src[0x11] << 8) | ((uint32_t)src[0x12] << 16);
+		dest = malloc(*out_size);
+		if (!dest) {
 			fprintf(stderr, "%s: malloc of 0x%" PRIX32 " bytes failed\n", __func__, *out_size);
 			return NULL;
 		}
-		if (!decompress_at6p_data(&in[0x14], data, *out_size)) {
-			free(data);
+		if (!decompress_at6p_data(&src[0x14], dest, *out_size)) {
+			free(dest);
 			return NULL;
 		}
-		return data;
+		return dest;
 	} else {
 		fprintf(stderr, "%s: Unknown file format\n", __func__);
 		return NULL;
 	}
 
-	if (in[4] == 'N') {
-		uint8_t *data = malloc(in_size);
-		if (!data) {
-			fprintf(stderr, "%s: malloc of 0x%" PRIX32 " bytes failed\n", __func__, in_size);
+	if (src[4] == 'N') {
+		dest = malloc(src_size);
+		if (!dest) {
+			fprintf(stderr, "%s: malloc of 0x%" PRIX32 " bytes failed\n", __func__, src_size);
 			return NULL;
 		}
-		return memcpy(data, &in[7], in_size);
+        *out_size = src_size;
+		return memcpy(dest, &src[7], src_size);
 	}
 
-	fprintf(stderr, "%s: Decompression of AT3P/AT4P/AT5P files is not implemented yet\n", __func__);
-	return NULL;
+    dest = malloc(*out_size);
+    if (!dest) {
+        fprintf(stderr, "%s: malloc of 0x%" PRIX32 " bytes failed\n", __func__, src_size);
+        return NULL;
+    }
+
+	const int xxxx_length = src[7] + 3;
+	const int xyyy_plus_length = src[8] + 3;
+	const int xyxx_minus_length = src[9] + 3;
+	const int xxyx_minus_length = src[0xA] + 3;
+	const int xxxy_minus_length = src[0xB] + 3;
+	const int xyyy_minus_length = src[0xC] + 3;
+	const int xyxx_plus_length = src[0xD] + 3;
+	const int xxyx_plus_length = src[0xE] + 3;
+	const int xxxy_plus_length = src[0xF] + 3;
+
+	for (; src_index < src_size; ++bit_index, flag_byte <<= 1) {
+		if (*out_size != 0 && dest_index >= *out_size) {
+			return 0;
+		}
+		if (bit_index == 8) {
+			// Read next flag byte
+			flag_byte = src[src_index];
+			++src_index;
+			bit_index = 0;
+		}
+		if ((flag_byte & 0x80) == 0) {
+			int lz_length = ((src[src_index] & 0xF0) >> 4) + 3;
+			int x = src[src_index] & 0xF;
+
+			if (lz_length == xxxx_length)       lz_length = 0x1F;
+			if (lz_length == xyyy_plus_length)  lz_length = 0x1E;
+			if (lz_length == xyxx_minus_length) lz_length = 0x1D;
+			if (lz_length == xxyx_minus_length) lz_length = 0x1C;
+			if (lz_length == xxxy_minus_length) lz_length = 0x1B;
+			if (lz_length == xyyy_minus_length) lz_length = 0x1A;
+			if (lz_length == xyxx_plus_length)  lz_length = 0x19;
+			if (lz_length == xxyx_plus_length)  lz_length = 0x18;
+			if (lz_length == xxxy_plus_length)  lz_length = 0x17;
+
+			switch (lz_length) {
+			case 0x1F:
+				// Emit XX XX
+				++src_index;
+				dest[dest_index] = x | (x << 4);
+				dest[dest_index + 1] = (x & 0xF) | ((x & 0xF) << 4);
+				dest_index += 2;
+				break;
+			case 0x1E:
+				// Y = X + 1; emit XY YY
+				++src_index;
+				int nybbleUp = (x + 1) & 0xF;
+				dest[dest_index] = nybbleUp | (x << 4);
+				dest[dest_index + 1] = nybbleUp | (nybbleUp << 4);
+				dest_index += 2;
+				break;
+			case 0x1D:
+				// Y = X - 1; emit XY XX
+				++src_index;
+				dest[dest_index] = ((x - 1) & 0xF) | (x << 4);
+				dest[dest_index + 1] = (x & 0xF) | (x << 4);
+				dest_index += 2;
+				break;
+			case 0x1C:
+				// Y = X - 1; emit XX YX
+				++src_index;
+				dest[dest_index] = x | (x << 4);
+				dest[dest_index + 1] = (((x - 1) & 0xF) << 4) | (x & 0xF);
+				dest_index += 2;
+				break;
+			case 0x1B:
+				// Y = X - 1; emit XX XY
+				++src_index;
+				dest[dest_index] = x | (x << 4);
+				dest[dest_index + 1] = (x << 4) | ((x - 1) & 0xF);
+				dest_index += 2;
+				break;
+			case 0x1A:
+				// Y = X - 1; emit XY YY
+				++src_index;
+				int nybbleDown = (x - 1) & 0xF;
+				dest[dest_index] = nybbleDown | (x << 4);
+				dest[dest_index + 1] = nybbleDown | (nybbleDown << 4);
+				dest_index += 2;
+				break;
+			case 0x19:
+				// Y = X + 1; emit XY XX
+				++src_index;
+				dest[dest_index] = ((x + 1) & 0xF) | (x << 4);
+				dest[dest_index + 1] = (x & 0xF) | (x << 4);
+				dest_index += 2;
+				break;
+			case 0x18:
+				// Y = X + 1; emit XX YX
+				++src_index;
+				dest[dest_index] = x | (x << 4);
+				dest[dest_index + 1] = (x & 0xF) | (((x + 1) & 0xF) << 4);
+				dest_index += 2;
+				break;
+			case 0x17:
+				// Y = X + 1; emit XX XY
+				++src_index;
+				dest[dest_index] = x | (x << 4);
+				dest[dest_index + 1] = ((x + 1) & 0xF) | ((x & 0xF) << 4);
+				dest_index += 2;
+				break;
+			default:;
+				int lz_index = (x << 8) + src[src_index + 1] + (dest_index - 0x1000);
+				src_index += 2;
+
+				for (int i = 0; i < lz_length; ++i) {
+					dest[dest_index++] = dest[lz_index++];
+				}
+				break;
+			}
+		} else {
+			dest[dest_index++] = src[src_index++];
+		}
+	}
+
+	return dest;
 }
 
 static void *read_all_bytes(FILE *f, size_t *f_len) {
@@ -190,7 +339,7 @@ static void *read_all_bytes(FILE *f, size_t *f_len) {
 	}
 	void *p = malloc(length);
 	if (!p) {
-		fprintf(stderr, "%s: malloc of 0x%" PRIX32 " bytes failed\n", __func__, length);
+		fprintf(stderr, "%s: malloc of 0x%lX bytes failed\n", __func__, length);
 		return NULL;
 	}
 	if (fread(p, 1, length, f) < length || fgetc(f) != EOF) {
@@ -202,15 +351,17 @@ static void *read_all_bytes(FILE *f, size_t *f_len) {
 	return p;
 }
 
-int main(int argc, char *argv[]) {
+int MAIN(int argc, CHAR *argv[]) {
 	if (argc < 3) {
-		fprintf(stderr, "Usage: %s infile.bin outfile.bin\n", argv[0] ? argv[0] : "999decompress");
+		OUTPUT_UNICODE_ON_WINDOWS();
+        FPRINTF(stderr, TEXT("Usage: %s infile.bin outfile.bin\n"), argv[0] ? argv[0] : TEXT("999decompress"));
 		return 1;
 	}
 
-	FILE *infile = fopen(argv[1], "rb");
+	FILE *infile = FOPEN(argv[1], TEXT("rb"));
 	if (!infile) {
-		fprintf(stderr, "Failed to open '%s' for reading\n", argv[1]);
+		OUTPUT_UNICODE_ON_WINDOWS();
+        FPRINTF(stderr, TEXT("Failed to open '%s' for reading\n"), argv[1]);
 		return 1;
 	}
 	size_t infile_length;
@@ -219,9 +370,10 @@ int main(int argc, char *argv[]) {
 		fclose(infile);
 		return 1;
 	}
-	FILE *outfile = fopen(argv[2], "wb");
+	FILE *outfile = FOPEN(argv[2], TEXT("wb"));
 	if (!outfile) {
-		fprintf(stderr, "Failed to open '%s' for writing\n", argv[2]);
+		OUTPUT_UNICODE_ON_WINDOWS();
+        FPRINTF(stderr, TEXT("Failed to open '%s' for writing\n"), argv[2]);
 		fclose(infile);
 		free(data);
 		return 1;
@@ -236,7 +388,8 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	if (fwrite(output, 1, out_size, outfile) != out_size) {
-		fprintf(stderr, "A problem occurred while writing to '%s'.\n", argv[2]);
+		OUTPUT_UNICODE_ON_WINDOWS();
+        FPRINTF(stderr, TEXT("A problem occurred while writing to '%s'.\n"), argv[2]);
 		fclose(infile);
 		fclose(outfile);
 		free(data);
@@ -244,7 +397,8 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	if (fclose(outfile)) {
-		fprintf(stderr, "A problem occurred while flushing writes to '%s'.\n", argv[2]);
+		OUTPUT_UNICODE_ON_WINDOWS();
+        FPRINTF(stderr, TEXT("A problem occurred while flushing writes to '%s'.\n"), argv[2]);
 		fclose(infile);
 		free(data);
 		free(output);
